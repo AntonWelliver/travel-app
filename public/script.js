55996c904a16f36dee65e1e185986005ccf0e4d1
Anton <antonwelliver27@gmail.com>
2020-01-05 20:15:25 +0100

    added js for calender and clock and some bootstrap classes

--- public/script.js
+++ public/script.js
@@ -128,8 +128,289 @@ dateTimeToggle.addEventListener("click", e => {
     if (dateTimeVisible === true) {
         dateTimeVisible = false;
         timeDateBox.classList.add("d-none");
+        submitButton.classList.remove("d-none");
     } else {
         dateTimeVisible = true;
         timeDateBox.classList.remove("d-none");
+        submitButton.classList.add("d-none");
     }
 });
+
+const datePickerElement = document.querySelector(".date-picker");
+const selectedDateElement = document.querySelector(
+    ".date-picker .selected-date"
+);
+const datesElement = document.querySelector(".date-picker .dates");
+const mthElement = document.querySelector(".date-picker .dates .month .mth");
+const nextMthElement = document.querySelector(
+    ".date-picker .dates .month .next-mth"
+);
+const prevMthElement = document.querySelector(
+    ".date-picker .dates .month .prev-mth"
+);
+const daysElement = document.querySelector(".date-picker .dates .days");
+
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+const weekDays = ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"];
+
+let date = new Date();
+let day = date.getDate();
+let month = date.getMonth();
+let year = date.getFullYear();
+let dayOfWeek = date.getDay();
+
+// The selected date
+let selectedDate = date;
+let selectedDay = day;
+let selectedMonth = month;
+let selectedYear = year;
+
+//mthElement.textContent = months[month] + " " + year;
+mthElement.textContent = `${months[month]} ${year}`;
+
+selectedDateElement.textContent = formatDate(date);
+selectedDateElement.dataset.value = selectedDate;
+
+populateDays();
+
+datePickerElement.addEventListener("click", e => {
+    if (checkEventPathForClass(e.path, "dates") != true) {
+        datesElement.classList.toggle("active");
+    }
+});
+
+nextMthElement.addEventListener("click", e => {
+    month++;
+    if (month > 11) {
+        month = 0;
+        year++;
+    }
+
+    mthElement.textContent = months[month] + " " + year;
+    populateDays();
+});
+
+prevMthElement.addEventListener("click", e => {
+    month--;
+    if (month < 0) {
+        month = 11;
+        year--;
+    }
+
+    mthElement.textContent = months[month] + " " + year;
+    populateDays();
+});
+
+function populateDays(e) {
+    daysElement.innerHTML = "";
+
+    let amountDays = 31;
+
+    switch (month) {
+        case 0: //Jan - 31
+            amountDays = 31;
+            break;
+        case 1: //Feb - 28
+            if (year % 4 === 0) {
+                amountDays = 29;
+            } else {
+                amountDays = 28;
+            }
+            break;
+        case 2: //Mar - 31
+            amountDays = 31;
+            break;
+        case 3: //Apr - 31
+            amountDays = 30;
+            break;
+        case 4: //May - 31
+            amountDays = 31;
+            break;
+        case 5: //Jun - 31
+            amountDays = 30;
+            break;
+        case 6: //Jul - 31
+            amountDays = 31;
+            break;
+        case 7: //Aug - 31
+            amountDays = 31;
+            break;
+        case 8: //Sep- 31
+            amountDays = 31;
+            break;
+        case 9: //Oct - 31
+            amountDays = 31;
+            break;
+        case 10: //Nov- 31
+            amountDays = 31;
+            break;
+        case 11: //Dec - 31
+            amountDays = 31;
+            break;
+
+        default:
+            amountDays = 31;
+    }
+
+    for (let i = 0; i < amountDays; i++) {
+        const dayElement = document.createElement("div");
+        dayElement.classList.add("day");
+        dayElement.textContent = i + 1;
+
+        if (
+            selectedDay == i + 1 &&
+            selectedYear == year &&
+            selectedMonth == month
+        ) {
+            dayElement.classList.add("selected");
+        }
+
+        // Select day from calendar
+        dayElement.addEventListener("click", e => {
+            selectedDate = new Date(year + "-" + (month + 1) + "-" + (i + 1));
+            selectedDay = i + 1;
+            selectedMonth = month;
+            selectedYear = year;
+            selectedDateElement.textContent = formatDate(selectedDate);
+            selectedDateElement.dataset.value = selectedDate;
+
+            populateDays();
+        });
+
+        daysElement.appendChild(dayElement);
+    }
+}
+
+function checkEventPathForClass(path, selector) {
+    for (let i = 0; i < path.length; i++) {
+        if (path[i].classList && path[i].classList.contains(selector)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function formatDate(d) {
+    let day = d.getDate();
+    if (day < 10) {
+        day = "0" + day;
+    }
+
+    let month = d.getMonth() + 1;
+    if (month < 10) {
+        month = "0" + month;
+    }
+    let year = d.getFullYear();
+
+    return day + " / " + month + " / " + year;
+}
+
+const timePickerElement = document.querySelector(".time-picker");
+
+const hrElement = document.querySelector(".time-picker .hour .hr");
+const minElement = document.querySelector(".time-picker .minute .min");
+
+const hrUp = document.querySelector(".time-picker .hour .hr-up");
+const hrDown = document.querySelector(".time-picker .hour .hr-down");
+
+const minUp = document.querySelector(".time-picker .minute .min-up");
+const minDown = document.querySelector(".time-picker .minute .min-down");
+
+let d = new Date();
+
+// The selected time
+let hour = d.getHours();
+let minute = d.getMinutes();
+setTime();
+
+// hour up cursor
+hrUp.addEventListener("click", () => {
+    hour++;
+    if (hour > 23) {
+        hour = 0;
+    }
+    setTime();
+});
+
+hrDown.addEventListener("click", () => {
+    hour--;
+    if (hour < 0) {
+        hour = 23;
+    }
+    setTime();
+});
+
+minDown.addEventListener("click", () => {
+    minute--;
+    if (minute < 0) {
+        minute = 59;
+        if (hour > 0) {
+            hour--;
+        }
+    }
+    setTime();
+});
+
+minUp.addEventListener("click", () => {
+    minute++;
+    if (minute > 59) {
+        minute = 0;
+        hour++;
+    }
+    setTime();
+});
+
+hrElement.addEventListener("change", e => {
+    if (e.target.value > 23) {
+        e.target.value = 23;
+    } else if (e.target.value < 0) {
+        e.target.value = "00";
+    }
+
+    if (e.target.value == "") {
+        e.target.value = formatTime(hour);
+    }
+
+    hour = e.target.value;
+});
+
+minElement.addEventListener("change", e => {
+    if (e.target.value > 59) {
+        e.target.value = 59;
+    } else if (e.target.value < 0) {
+        e.target.value = "00";
+    }
+
+    if (e.target.value == "") {
+        e.target.value = formatTime(minute);
+    }
+
+    minute = e.target.value;
+});
+
+function setTime() {
+    hrElement.value = formatTime(hour);
+    minElement.value = formatTime(minute);
+    timePickerElement.dataset.time = formatTime(hour) + ":" + formatTime(minute);
+}
+
+function formatTime(time) {
+    if (time < 10) {
+        time = "0" + time;
+    }
+    return time;
+}
